e7fa04efbad7ee9c3d6420dddcc99597f7b9ceb0
Yuri Ramos <yuriramos2406@gmail.com>
2025-11-18 17:12:39 -0300

    feat: remove console logging configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Use o objeto `compiler.removeConsole` para que o SWC remova `console.*` no build de produção
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
